98da052b592f905a9e5e88282847b71a780b8803
andreynho2006 <andreynho2006@gmail.com>
2018-08-23 15:05:21 +0300

    add class prepare thumb that adds class lightbox to all li elements and eventlistener on click

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,5 +1,31 @@
 function init() {
-    window.console.log("Welcome, user with a new browser");
+    var lightboxElements = "<div id='lightbox'>";
+    lightboxElements += "<div id='overlay' class='hidden'></div>";
+    lightboxElements += "<img class='hidden' id='big-image' />";
+    lightboxElements == "</div>";
+    document.querySelector("body").innerHTML += lightboxElements;
+    prepareThumbs();
 }
+
+function toggle() {
+    window.console.log("show or hide a big image");
+}
+
+function prepareThumbs() {
+    var liElements = document.querySelectorAll("ul#images li");
+    var i = 0;
+    var image, li;
+    //loop through all <li> elements
+    while ( i < liElements.length ) {
+        li = liElements[i];
+        //set class='lightbox'
+        li.setAttribute("class", "lightbox");
+        image = li.querySelector("img");
+        //register a click event handler for the <img> elements
+        image.addEventListener("click", toggle, false);
+        i += 1;
+    }
+}
+
 
 document.addEventListener("DOMContentLoaded", init, false);
